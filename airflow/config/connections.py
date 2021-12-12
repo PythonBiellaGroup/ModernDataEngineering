bc77f76eb60498bc884300a3ba95e2aeb8659c99
JeyDi <andrea.guzzo92@gmail.com>
2021-12-12 16:30:39 +0100

    start working on airflow environment

---
+++ airflow/config/connections.py
@@ -0,0 +1,20 @@
+import yaml
+from airflow import settings
+from airflow.models import Connection
+
+with open("connections.yml", "r") as f:
+    connection_dict = yaml.safe_load(f.read())
+for connection_name, connection in connection_dict.items():
+    conn = Connection(
+        conn_id=connection_name,
+        conn_type=connection.get('conn_type'),
+        host=connection.get('host'),
+        login=connection.get('login'),
+        password=connection.get('password'),
+        port=connection.get('port'),
+        schema=connection.get('schema'),
+        extra=connection.get('extra')
+    ) 
+    session = settings.Session
+    session.add(conn)
+    session.commit()
