ea7aea675fd543e5330f22af22437d6bc312cf55
JeyDi <andrea.guzzo92@gmail.com>
2022-03-31 23:11:47 +0200

    materiale serata 1

--- airflow/dags/python_example.py
+++ airflow/dags/python_example.py
@@ -12,6 +12,7 @@ args = {
     "retry_delay": timedelta(minutes=1),
     "retries": 1,
 }
+
 dag = DAG(
     dag_id="python_sample_dag",
     start_date=datetime.datetime.now(),
@@ -25,14 +26,21 @@ def run_this_func():
 
 
 def run_also_this_func():
+    print("Ciao Python Biella Group")
     print("I am coming last")
+
+
+def hello_PBG():
+    print("Ciao Python Biella Group")
 
 
 with dag:
     run_this_task = PythonOperator(
         task_id="run_this_first", python_callable=run_this_func
     )
-run_this_task_too = PythonOperator(
-    task_id="run_this_last", python_callable=run_also_this_func
-)
-run_this_task >> run_this_task_too
+    run_this_task_too = PythonOperator(
+        task_id="run_this_last", python_callable=run_also_this_func
+    )
+    run_PBG = PythonOperator(task_id="run_PBG", python_callable=run_also_this_func)
+
+run_this_task >> run_this_task_too >> run_PBG
---
+++ airflow/dags/serata_1.py
@@ -0,0 +1,119 @@
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.exceptions import AirflowSensorTimeout
+from airflow.operators.dummy import DummyOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
+
+FILE_PATH = "/opt/airflow/data"
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.now(),
+    "email": ["pythonbiellagroup@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+}
+
+
+# Operators configuration
+def _failure_callback(context):
+    if isinstance(context["exception"], AirflowSensorTimeout):
+        print(context)
+    print("Sensor timed out")
+
+
+def save_data(df: pd.DataFrame, filename: str = "data.csv", data_path: str = FILE_PATH):
+    data_path = data_path
+    data_file = filename
+    file_path = os.path.join(data_path, data_file)
+    df.to_csv(file_path, index=False)
+    return df
+
+
+def extract_data(filename: str = "extracted.csv"):
+    df = pd.read_excel(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx",
+        nrows=1000,
+    )
+    df = save_data(df, filename=filename)
+    return df
+
+
+def transform_data(df: pd.DataFrame, filename: str = "transformed.csv"):
+    file_name = os.path.join(FILE_PATH, filename)
+    df = pd.read_csv(file_name)
+    df["TotalAmount"] = df["UnitPrice"] * df["Quantity"]
+    df.drop(
+        columns=["StockCode", "Description", "Country", "UnitPrice", "Quantity"],
+        inplace=True,
+    )
+    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"]).dt.date
+    df["CustomerID"] = df["CustomerID"].astype("Int64")
+    df = save_data(df, filename=filename)
+    return df
+
+
+def group_data(df: pd.DataFrame, filename: str = "aggregated.csv"):
+    file_name = os.path.join(FILE_PATH, filename)
+    df = pd.read_csv(file_name)
+    df_aggr = df.groupby(["InvoiceNo", "InvoiceDate", "CustomerID"])[
+        "TotalAmount"
+    ].sum()
+    save_data(df_aggr, filename=filename)
+    return df
+
+
+dag = DAG(
+    "pipeline_serata",
+    start_date=datetime.now(),
+    schedule_interval="@daily",
+    catchup=False,
+    default_args=default_args,
+)
+
+run_bash_extractor = BashOperator(
+    task_id="bash_extractor",
+    bash_command="python /opt/airflow/dags/jobs/process_data.py extract_data",
+    dag=dag,
+)
+
+run_python_extractor = PythonOperator(
+    task_id="python_extractor", python_callable=extract_data, dag=dag
+)
+
+run_python_transform = PythonOperator(
+    task_id="python_transform", python_callable=transform_data, dag=dag
+)
+
+run_python_group = PythonOperator(
+    task_id="python_group", python_callable=group_data, dag=dag
+)
+
+sensor_extract = FileSensor(
+    task_id="sensor_extract",
+    mode="reschedule",
+    on_failure_callback=_failure_callback,
+    filepath="/opt/airflow/data/extracted.csv",
+    poke_interval=15,
+    timeout=15 * 60,
+    fs_conn_id="conn_filesensor_extract",
+)
+
+start_op = DummyOperator(task_id="start_task", dag=dag)
+mid_op = DummyOperator(task_id="mid_task", dag=dag)
+last_op = DummyOperator(task_id="last_task", dag=dag)
+
+# run_this_task_too = PythonOperator(
+#     task_id="run_this_last", python_callable=run_also_this_func
+# )
+
+start_op >> run_python_extractor >> sensor_extract >> [run_python_transform, run_python_group] >> last_op
